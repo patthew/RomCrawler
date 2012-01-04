188895142669c7aedd7ecb8ebf7a61d8920bcdd4
lennysh <ircmaster@hotmail.com>
2012-01-04 14:26:55 -0500

    Update src/com/t3hh4xx0r/romcrawler/DeviceType.java

--- src/com/t3hh4xx0r/romcrawler/DeviceType.java
+++ src/com/t3hh4xx0r/romcrawler/DeviceType.java
@@ -2,6 +2,7 @@ package com.t3hh4xx0r.romcrawler;
 
 public enum DeviceType {
 	   TORO("http://rootzwiki.com/forum/362-cdma-galaxy-nexus-developer-forum");
+	   VIGOR("http://rootzwiki.com/forum/267-rezound-developer-forum/");
 
 	    private final String forumUrl;
 
